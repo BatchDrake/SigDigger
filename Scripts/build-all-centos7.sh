587d10a3d2b01cb2a2b968d7a00ff3afb8a04f66
Gonzalo José Carracedo Carballal <BatchDrake@gmail.com>
2020-03-07 20:06:26 +0100

    Fix build-all script for CentOS to include local libraries

--- Scripts/build-all-centos7.sh
+++ Scripts/build-all-centos7.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 scl enable devtoolset-8 -- ./dist-appimage.sh
 SIGDIGGER_EMBED_SOAPYSDR=1 scl enable devtoolset-8 -- ./dist-appimage.sh
