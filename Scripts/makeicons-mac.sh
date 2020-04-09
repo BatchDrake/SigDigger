9af8da9190af18249508e85aa38205f89594965e
Gonzalo J. Carracedo Carballal <BatchDrake@gmail.com>
2020-04-09 23:59:01 +0200

    Add script to generate iconset in Mac

---
+++ Scripts/makeicons-mac.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ICONPATH=../icons/icon-256x256.png
+
+mkdir SigDigger.iconset
+sips -z 16 16     "$ICONPATH" --out SigDigger.iconset/icon_16x16.png
+sips -z 32 32     "$ICONPATH" --out SigDigger.iconset/icon_16x16@2x.png
+sips -z 32 32     "$ICONPATH" --out SigDigger.iconset/icon_32x32.png
+sips -z 64 64     "$ICONPATH" --out SigDigger.iconset/icon_32x32@2x.png
+sips -z 128 128   "$ICONPATH" --out SigDigger.iconset/icon_128x128.png
+sips -z 256 256   "$ICONPATH" --out SigDigger.iconset/icon_128x128@2x.png
+sips -z 256 256   "$ICONPATH" --out SigDigger.iconset/icon_256x256.png
+sips -z 512 512   "$ICONPATH" --out SigDigger.iconset/icon_256x256@2x.png
+sips -z 512 512   "$ICONPATH" --out SigDigger.iconset/icon_512x512.png
+sips -z 1024 1024 "$ICONPATH" --out SigDigger.iconset/icon_512x512@2x.png
+
+iconutil -c icns SigDigger.iconset
