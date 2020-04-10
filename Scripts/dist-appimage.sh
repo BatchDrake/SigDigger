c8cfa5951ca2f443c0b4549d6b079fa09ef707be
Gonzalo J. Carracedo Carballal <BatchDrake@gmail.com>
2020-04-10 14:48:12 +0200

    Add script to create .dmg files

--- Scripts/dist-appimage.sh
+++ Scripts/dist-appimage.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #
-#  appimage.sh: Deploy SigDigger in AppImage format
+#  dist-appimage.sh: Deploy SigDigger in AppImage format
 #
 #  The following environment variables adjust the behavior of this script:
 #
-#    SIGDIGGER_SKIPBUILD: Skip cloning and build.
-#   
 #    SIGDIGGER_EMBED_SOAPYSDR: Embeds SoapySDR to the resulting AppImage,
 #      along with all the modules installed in the deployment system.
 #
@@ -27,46 +25,9 @@
 #
 #
 
-DISTROOT="$PWD"
-BRANCH=develop
-SCRIPTPATH=`realpath $0`
-SCRIPTDIR=`dirname "$SCRIPTPATH"`
-APPIMAGEROOT="$DISTROOT/appimage-root"
-BUILDROOT="$DISTROOT/appimage-buildroot"
-THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+. dist-common.sh
+
 SRC_APPIMAGE_NAME=SigDigger-`uname -m`.AppImage
-
-
-function try()
-{
-    echo -en "[  ....  ] $1 "
-    shift
-    
-    STDOUT="$DISTROOT/$1-$$-stdout.log"
-    STDERR="$DISTROOT/$1-$$-stderr.log"
-    "$@" > "$STDOUT" 2> "$STDERR"
-    
-    if [ $? != 0 ]; then
-	echo -e "\r[ \033[1;31mFAILED\033[0m ]"
-	echo
-	echo "Standard output and error were saved respectively in:"
-	echo " - $STDOUT"
-	echo " - $STDERR"
-	echo
-	echo "Fix errors and try again"
-	echo
-	exit 1
-    fi
-
-    echo -e "\r[   \033[1;32mOK\033[0m   ]"
-    rm "$STDOUT"
-    rm "$STDERR"
-}
-
-function skip()
-{
-    echo -e "[  \033[1;33mSKIP\033[0m  ] $1"
-}
 
 function update_excludelist()
 {
@@ -115,26 +76,26 @@ function assert_symlink()
 
 function embed_soapysdr()
 {
-    SOAPYSDRVER=`ldd $APPIMAGEROOT/usr/bin/SigDigger | grep Soapy | sed 's/ =>.*$//g' | sed 's/^.*\.so\.//g'`
+    SOAPYSDRVER=`ldd $DEPLOYROOT/usr/bin/SigDigger | grep Soapy | sed 's/ =>.*$//g' | sed 's/^.*\.so\.//g'`
     try "Testing SoapySDR version..." [ "$SOAPYSDRVER" != "" ]
     try "Testing SoapySDR dir..." find_soapysdr
 
     MODDIR=`find_soapysdr`
 
-    try "Creating symlinks..."  assert_symlink . "$APPIMAGEROOT"/usr/lib/`uname -m`-linux-gnu
-    try "Creating symlinks..." assert_symlink lib "$APPIMAGEROOT"/usr/lib64
+    try "Creating symlinks..."  assert_symlink . "$DEPLOYROOT"/usr/lib/`uname -m`-linux-gnu
+    try "Creating symlinks..." assert_symlink lib "$DEPLOYROOT"/usr/lib64
 
-    try "Creating SoapySDR module dir..." mkdir -p "$APPIMAGEROOT"/usr/lib/SoapySDR
-    try "Copying SoapySDR modules ($MODDIR)..." cp -Rfv "$MODDIR" "$APPIMAGEROOT"/usr/lib/SoapySDR
+    try "Creating SoapySDR module dir..." mkdir -p "$DEPLOYROOT"/usr/lib/SoapySDR
+    try "Copying SoapySDR modules ($MODDIR)..." cp -Rfv "$MODDIR" "$DEPLOYROOT"/usr/lib/SoapySDR
     
-    RADIOS=`ldd "$APPIMAGEROOT"/usr/lib/SoapySDR/modules$SOAPYSDRVER/lib* | grep '=>' | sed 's/^.*=> \(.*\) .*$/\1/g' | tr -d '[ \t]' | sort | uniq`
+    RADIOS=`ldd "$DEPLOYROOT"/usr/lib/SoapySDR/modules$SOAPYSDRVER/lib* | grep '=>' | sed 's/^.*=> \(.*\) .*$/\1/g' | tr -d '[ \t]' | sort | uniq`
     
     for i in $RADIOS; do
 	name=`basename "$i"`
-	if [ ! -f "$APPIMAGEROOT"/usr/lib/"$name" ] && ! excluded "$name"; then
-	    try "Bringing $name..." cp -L "$i" "$APPIMAGEROOT"/usr/lib
+	if [ ! -f "$DEPLOYROOT"/usr/lib/"$name" ] && ! excluded "$name"; then
+	    try "Bringing $name..." cp -L "$i" "$DEPLOYROOT"/usr/lib
 	else
-	    rm -f "$APPIMAGEROOT"/usr/lib/"$name"
+	    rm -f "$DEPLOYROOT"/usr/lib/"$name"
 	    skip "Skipping $name..."
 	fi
     done
@@ -142,65 +103,26 @@ function embed_soapysdr()
 
 update_excludelist
 
-if [ "$SIGDIGGER_SKIPBUILD" == "" ]; then
-    try "Cleaning deploy directory..." rm -Rfv "$APPIMAGEROOT"
-    try "Cleaning buildroot..."        rm -Rfv "$BUILDROOT"
-    try "Recreating directories..."    mkdir -p "$APPIMAGEROOT" "$BUILDROOT"
+build
 
-    cd "$BUILDROOT"
-    export PKG_CONFIG_PATH="$APPIMAGEROOT/usr/lib/pkgconfig:$PKG_CONFIG"
-    export LD_LIBRARY_PATH="$APPIMAGEROOT/usr/lib:$LD_LIBRARY_PATH"
+try "Creating appdir..."    mkdir -p "$DEPLOYROOT"/usr/share/applications
+try "Creating metainfo..."  mkdir -p "$DEPLOYROOT"/usr/share/metainfo
+try "Copying metainfo..."   cp "$SCRIPTDIR/SigDigger.appdata.xml" "$DEPLOYROOT"/usr/share/metainfo/org.actinid.SigDigger.xml
+try "Creating icondir..."   mkdir -p "$DEPLOYROOT"/usr/share/icons/hicolor/256x256/apps
 
-    try "Cloning sigutils..."          git clone -b "$BRANCH" https://github.com/BatchDrake/sigutils
-    try "Cloning suscan..."            git clone -b "$BRANCH" https://github.com/BatchDrake/suscan
-    try "Cloning SuWidgets..."         git clone -b "$BRANCH" https://github.com/BatchDrake/SuWidgets
-    try "Cloning SigDigger..."         git clone -b "$BRANCH" https://github.com/BatchDrake/SigDigger
-    try "Creating builddirs..."        mkdir -p sigutils/build suscan/build
-    cd sigutils/build
-    try "Running CMake (sigutils)..."  cmake .. -DCMAKE_INSTALL_PREFIX="$APPIMAGEROOT/usr"  -DCMAKE_BUILD_TYPE=Release -DCMAKE_SKIP_RPATH=ON -DCMAKE_SKIP_INSTALL_RPATH=ON
-    cd ../../
-    try "Building sigutils..."         make -j $THREADS -C sigutils/build
-    try "Deploying sigutils..."        make -j $THREADS -C sigutils/build install
-
-    cd suscan/build
-    try "Running CMake (suscan)..."    cmake .. -DCMAKE_INSTALL_PREFIX="$APPIMAGEROOT/usr"  -DCMAKE_BUILD_TYPE=Release -DCMAKE_SKIP_RPATH=ON -DCMAKE_SKIP_INSTALL_RPATH=ON -DSUSCAN_PKGDIR="/usr"
-    cd ../../
-    try "Building suscan..."           make -j $THREADS -C suscan/build
-    try "Deploying suscan..."          make -j $THREADS -C suscan/build install
-
-    cd SuWidgets
-    try "Running QMake (SuWidgets)..." qmake SuWidgetsLib.pro "CONFIG += release" SUWIDGETS_PREFIX="$APPIMAGEROOT/usr"
-    try "Building SuWidgets..."        make -j $THREADS
-    try "Deploying SuWidgets..."       make install
-    cd ..
-
-    cd SigDigger
-    try "Running QMake (SigDigger)..." qmake SigDigger.pro "CONFIG += release" SUWIDGETS_PREFIX="$APPIMAGEROOT/usr" SIGDIGGER_PREFIX="$APPIMAGEROOT/usr"
-    try "Building SigDigger..."        make -j $THREADS
-    try "Deploying SigDigger..."       make install
-    cd ..
-else
-    echo "Skipping build..."
-fi
-
-try "Creating appdir..."    mkdir -p "$APPIMAGEROOT"/usr/share/applications
-try "Creating metainfo..."  mkdir -p "$APPIMAGEROOT"/usr/share/metainfo
-try "Copying metainfo..."   cp "$SCRIPTDIR/SigDigger.appdata.xml" "$APPIMAGEROOT"/usr/share/metainfo/org.actinid.SigDigger.xml
-try "Creating icondir..."   mkdir -p "$APPIMAGEROOT"/usr/share/icons/hicolor/256x256/apps
-
-try "Copying icons..." cp "$BUILDROOT"/SigDigger/icons/icon-256x256.png "$APPIMAGEROOT"/usr/share/icons/hicolor/256x256/apps/SigDigger.png
+try "Copying icons..." cp "$BUILDROOT"/SigDigger/icons/icon-256x256.png "$DEPLOYROOT"/usr/share/icons/hicolor/256x256/apps/SigDigger.png
 echo "[Desktop Entry]
 Type=Application
 Name=SigDigger
 Comment=The Free Digital Signal Analyzer
 Exec=SigDigger
 Icon=SigDigger
-Categories=HamRadio;" > "$APPIMAGEROOT"/usr/share/applications/SigDigger.desktop
+Categories=HamRadio;" > "$DEPLOYROOT"/usr/share/applications/SigDigger.desktop
 
-try "Removing unneeded development files..." rm -Rfv "$APPIMAGEROOT"/usr/include "$APPIMAGEROOT"/usr/bin/suscan.status "$APPIMAGEROOT"/usr/lib/pkgconfig
+try "Removing unneeded development files..." rm -Rfv "$DEPLOYROOT"/usr/include "$DEPLOYROOT"/usr/bin/suscan.status "$DEPLOYROOT"/usr/lib/pkgconfig
 
-if [ -f "$APPIMAGEROOT"/usr/bin/SigDigger.app ]; then
-    try "Restoring old SigDigger executable..." cp "$APPIMAGEROOT"/usr/bin/SigDigger.app "$APPIMAGEROOT"/usr/bin/SigDigger
+if [ -f "$DEPLOYROOT"/usr/bin/SigDigger.app ]; then
+    try "Restoring old SigDigger executable..." cp "$DEPLOYROOT"/usr/bin/SigDigger.app "$DEPLOYROOT"/usr/bin/SigDigger
 fi
 
 if [ "$SIGDIGGER_EMBED_SOAPYSDR" != "" ]; then
@@ -210,9 +132,9 @@ else
     APPIMAGE_NAME=SigDigger-lite-`uname -m`.AppImage
 fi
 
-try "Calling linuxdeployqt..." linuxdeployqt "$APPIMAGEROOT"/usr/share/applications/SigDigger.desktop -bundle-non-qt-libs
+try "Calling linuxdeployqt..." linuxdeployqt "$DEPLOYROOT"/usr/share/applications/SigDigger.desktop -bundle-non-qt-libs
 
-try "Moving SigDigger binary..." mv "$APPIMAGEROOT"/usr/bin/SigDigger "$APPIMAGEROOT"/usr/bin/SigDigger.app
+try "Moving SigDigger binary..." mv "$DEPLOYROOT"/usr/bin/SigDigger "$DEPLOYROOT"/usr/bin/SigDigger.app
 
 if [ "$SIGDIGGER_EMBED_SOAPYSDR" != "" ]; then
     echo '#!/bin/sh
@@ -224,15 +146,15 @@ if [ "x$SIGDIGGER_SOAPY_SDR_ROOT" != "x" ]; then
   export SOAPY_SDR_ROOT="$SIGDIGGER_SOAPY_SDR_ROOT"
 fi
 export LD_LIBRARY_PATH="${HERE}/../lib:$LD_LIBRARY_PATH"
-exec "${HERE}"/SigDigger.app "$@"' > "$APPIMAGEROOT"/usr/bin/SigDigger
+exec "${HERE}"/SigDigger.app "$@"' > "$DEPLOYROOT"/usr/bin/SigDigger
 else
     echo '#!/bin/sh
 SELF=$(readlink -f "$0")
 HERE=${SELF%/*}
 export SUSCAN_CONFIG_PATH="${HERE}/../share/suscan/config"
 export LD_LIBRARY_PATH="${HERE}/../lib:$LD_LIBRARY_PATH"
-exec "${HERE}"/SigDigger.app "$@"' > "$APPIMAGEROOT"/usr/bin/SigDigger
+exec "${HERE}"/SigDigger.app "$@"' > "$DEPLOYROOT"/usr/bin/SigDigger
 fi
-try "Setting permissions to wrapper script..." chmod a+x "$APPIMAGEROOT"/usr/bin/SigDigger
-try "Calling AppImageTool and finishing..." appimagetool "$APPIMAGEROOT"
+try "Setting permissions to wrapper script..." chmod a+x "$DEPLOYROOT"/usr/bin/SigDigger
+try "Calling AppImageTool and finishing..." appimagetool "$DEPLOYROOT"
 try "Renaming to $APPIMAGE_NAME..." mv "$SRC_APPIMAGE_NAME" "$DISTROOT/$APPIMAGE_NAME"
---
+++ Scripts/dist-dmg.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+#  dist-dmg.sh: Deploy SigDigger in MacOS disk image format
+#
+#  The following environment variables adjust the behavior of this script:
+#
+#    SIGDIGGER_EMBED_SOAPYSDR: Embeds SoapySDR to the resulting AppImage,
+#      along with all the modules installed in the deployment system.
+#
+#  Copyright (C) 2020 Gonzalo José Carracedo Carballal
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this program.  If not, see
+#  <http://www.gnu.org/licenses/>
+#
+#
+
+. dist-common.sh
+
+BUNDLEPATH="$DEPLOYROOT/usr/bin/SigDigger.app"
+RSRCPATH="$BUNDLEPATH/Contents/Resources"
+LIBPATH="$BUNDLEPATH/Contents/Frameworks"
+STAGINGDIR="$DEPLOYROOT/SigDigger.dir"
+
+function locate_macdeploy()
+{
+  locate_qt
+  
+  export PATH="$QT_BIN_PATH:$PATH"
+  
+  try "Locating hdiutil..." which hdiutil
+  try "Locating macdeployqt..." which macdeployqt
+}
+
+function bundle_libs()
+{
+  name="$1"
+  shift
+  
+  try "Bundling $name..." cp -Rfv "$@" "$LIBPATH"
+}
+
+#
+# If at some point, SigDigger needs additional libraries, expand this function
+# adding the missing dependencies with bundle_libs.
+#
+# bundle_libs accepts the name of the set of libraries (for informative
+# purposes only) and a set of libraries.
+#
+
+function deploy_deps()
+{
+  bundle_libs "SoapySDR libraries" /usr/local/lib/libSoapySDR*dylib
+}
+
+function create_dmg()
+{
+  try "Creating staging directory..." mkdir -p "$STAGINGDIR"
+  try "Copying bundle to staging dir..."   cp -Rfv "$BUNDLEPATH" "$STAGINGDIR"
+  try "Creating .dmg file and finishing..." hdiutil create -volname SigDigger -srcfolder "$STAGINGDIR" -ov -format UDZO "$DISTROOT"/SigDigger.dmg
+}
+
+function deploy()
+{
+  locate_macdeploy
+  try "Deploying via macdeployqt..." macdeployqt "$BUNDLEPATH"
+  try "Copying Suscan data directory to bundle..." cp -Rfv "$DEPLOYROOT/usr/share/suscan" "$RSRCPATH" 
+  try "Bundling built libraries..." cp -Rfv "$DEPLOYROOT/usr/lib/"*.dylib "$LIBPATH"
+  
+  deploy_deps
+}
+
+build
+deploy
+create_dmg
+
+
+
+
