e6af4ab0b4b7bdf002e00b2e99904d7560f52a56
Gonzalo José Carracedo Carballal <BatchDrake@gmail.com>
2022-04-12 10:41:51 +0200

    WIP: Add missing files to project

---
+++ Suscan/Plugin.cpp
@@ -0,0 +1,234 @@
+//
+//    Plugin.cpp: Plugin support
+//    Copyright (C) 2022 Gonzalo José Carracedo Carballal
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful, but
+//    WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this program.  If not, see
+//    <http://www.gnu.org/licenses/>
+//
+
+#define SU_LOG_DOMAIN "sigdigger-plugin"
+
+#include "Plugin.h"
+#include <cstdio>
+#include <dlfcn.h>
+#include <Suscan/Logger.h>
+#include <SuWidgetsHelpers.h>
+#include <Version.h>
+#include <FeatureFactory.h>
+
+using namespace Suscan;
+
+Plugin *Plugin::m_default = nullptr;
+
+// Static members
+Plugin *
+Plugin::make(const char *path)
+{
+  void *handle = nullptr;
+  Plugin *plugin = nullptr;
+  const char *name;
+  const char *desc;
+
+  if ((handle = dlopen(path, RTLD_LAZY)) == nullptr) {
+    SU_ERROR("Cannot open %s: %s\n", path, dlerror());
+    return nullptr;
+  }
+
+  // Not an error, just not a plugin
+  if ((name = SCAST(const char *, dlsym(handle, "plugin_name"))) == nullptr)
+    goto done;
+
+  if ((desc = SCAST(const char *, dlsym(handle, "plugin_desc"))) == nullptr)
+    goto done;
+
+  plugin = new Plugin(name, path, desc, handle);
+
+done:
+  if (plugin == nullptr && handle != nullptr)
+    dlclose(handle);
+
+  return plugin;
+}
+
+Plugin *
+Plugin::getDefaultPlugin()
+{
+  if (m_default == nullptr) {
+    m_default = new Plugin("default", "N/A", "Default plugin", nullptr);
+
+    m_default->m_version = SIGDIGGER_VERSION_STRING;
+    m_default->m_loaded = true;
+  }
+
+  return m_default;
+}
+
+// Implementation
+Plugin::Plugin()
+{
+
+}
+
+Plugin::~Plugin()
+{
+  if (this->m_loaded) {
+    fprintf(
+          stderr,
+          "Critical: cannot destroy plugin `%s': plugin is loaded\n",
+          this->m_name.c_str());
+    abort();
+  }
+
+  if (this->m_handle != nullptr)
+    dlclose(this->m_handle);
+}
+
+void *
+Plugin::resolveSym(std::string const &sym)
+{
+  void *addr;
+  std::string full_name =
+      STRINGIFY(SUSCAN_SYM_PFX) + sym;
+
+  addr = dlsym(this->m_handle, full_name.c_str());
+
+  if (addr == nullptr) {
+    SU_ERROR(
+          "%s: cannot resolve plugin symbol `%s'\n",
+          this->m_path.c_str(),
+          sym.c_str());
+    return nullptr;
+  }
+
+  return addr;
+}
+
+Plugin::Plugin(
+    std::string const &name,
+    std::string const &path,
+    std::string const &desc,
+    void *handle)
+{
+  this->m_handle      = handle;
+  this->m_name        = name;
+  this->m_path        = path;
+  this->m_description = desc;
+}
+
+bool
+Plugin::load(void)
+{
+  PluginEntryFunc pluginEntry =
+      reinterpret_cast<PluginEntryFunc>(this->resolveSym("plugin_load"));
+  const uint32_t *pPluginVer =
+      SCAST(const uint32_t *, this->resolveSym("plugin_ver"));
+  const uint32_t *pAPIVer    =
+      SCAST(const uint32_t *, this->resolveSym("api_ver"));
+
+  if (this->m_loaded)
+    return true;
+
+  if (pPluginVer == nullptr || pAPIVer == nullptr || pluginEntry == nullptr)
+    return false;
+
+  this->m_version = QString::asprintf(
+        "%d.%d.%d",
+        0xff & (*pPluginVer >> 16),
+        0xff & (*pPluginVer >> 8),
+        0xff & (*pPluginVer >> 0)).toStdString();
+
+  if (*pAPIVer > SIGDIGGER_API_VERSION) {
+    SU_ERROR(
+          "%s: this plugin is not compatible with the current SigDigger version\n",
+          this->m_path.c_str());
+    return false;
+  }
+
+  this->m_loaded = (pluginEntry) (this);
+
+  return true;
+}
+
+bool
+Plugin::unload(void)
+{
+  if (this->m_loaded) {
+    if (!this->canBeUnloaded()) {
+      SU_ERROR(
+            "%s: plugin cannot be loaded (plugin in use)\n",
+            this->m_path.c_str());
+      return false;
+    }
+
+    auto p = this->m_factorySet.begin();
+
+    // Erase and unregister all factories globally
+    while (p != this->m_factorySet.end()) {
+      FeatureFactory *ref = *p;
+      if (!ref->unregisterGlobally()) {
+        SU_ERROR(
+              "%s: plugin cannot be loaded (failed to unregister global references to %s)\n",
+              this->m_path.c_str(),
+              ref->name());
+        return false;
+      }
+
+      // Erase and get next. It is important to do this before deleting
+      // the object.
+      p = this->m_factorySet.erase(p);
+
+      delete ref;
+    }
+
+    this->m_loaded = false;
+  }
+
+  return true;
+}
+
+bool
+Plugin::registerFactory(FeatureFactory *factory)
+{
+  if (this->m_factorySet.contains(factory)) {
+    SU_ERROR(
+          "%s: attempting to register a %s factory twice\n",
+          this->m_path.c_str(),
+          factory->name());
+    return false;
+  }
+
+  this->m_factorySet.insert(factory);
+
+  return true;
+}
+
+bool
+Plugin::unregisterFactory(FeatureFactory *factory)
+{
+  // This is not an error: removal may have been triggered by erase
+  if (this->m_factorySet.contains(factory))
+    this->m_factorySet.remove(factory);
+
+  return true;
+}
+
+bool
+Plugin::canBeUnloaded(void) const
+{
+  for (auto p : this->m_factorySet)
+    if (!p->canBeRemoved())
+      return false;
+
+  return true;
+}
