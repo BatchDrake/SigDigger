09782934f8d7f44e8a9fa02cce2035313e9839e1
Gonzalo J. Carracedo Carballal <Batchdrake@gmail.com>
2024-06-05 12:31:25 +0200

    Fix numerical roundup issues in spectrum representation

--- Default/GenericInspector/GenericInspector.cpp
+++ Default/GenericInspector/GenericInspector.cpp
@@ -235,7 +235,7 @@ GenericInspector::inspectorMessage(Suscan::InspectorMessage const &msg)
       p = len / 2;
 
       for (auto i = 0u; i < len; ++i)
-        data[i] = SU_POWER_DB_RAW(data[i]);
+        data[i] = SU_POWER_DB_RAW(data[i] + 1e-20f);
 
       for (auto i = 0u; i < len / 2; ++i) {
         x = data[i];
